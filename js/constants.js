2cda102cc95a5073b17053a33b89825820d98109
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2024-03-01 01:56:45 +0100

    Add themeKey constant and update themeToggler.js

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,2 @@
-export const url = "https://exam.mittportfolio.com/wp-json/wp/v2"
+export const url = "https://exam.mittportfolio.com/wp-json/wp/v2";
+export const themeKey = "theme";
--- js/themeToggler.js
+++ js/themeToggler.js
@@ -1,3 +1,4 @@
+import * as constants from "./constants.js";
 import { loadFromLocalStorage, saveToLocalStorage, removeFromLocalStorage } from "./storage/local.js";
 
 // Export function to toggle between light and dark theme
@@ -11,16 +12,16 @@ export function toggleTheme() {
             const theme = document.body;
             theme.classList.toggle('dark-mode');
             if (theme.classList.contains('dark-mode')) {
-                removeFromLocalStorage('theme');
-                saveToLocalStorage('theme', theme.classList);
+                removeFromLocalStorage(constants.themeKey);
+                saveToLocalStorage(constants.themeKey, theme.classList);
             } else {
-                removeFromLocalStorage('theme');
+                removeFromLocalStorage(constants.themeKey);
             }
         });
     });
 
     // Load the theme from local storage and set <label class="toggleswitch"> <input type="checkbox"><span class="fas fa-sun slider round"></span></label> input to checked if dark mode is enabled
-    const theme = loadFromLocalStorage('theme');
+    const theme = loadFromLocalStorage(constants.themeKey);
     if (theme) {
         document.body.classList = theme[0];
         if (theme[0] === 'dark-mode') {
