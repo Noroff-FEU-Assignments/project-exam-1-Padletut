a19cb383709a24334f7bee38c741403805c59009
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2024-03-04 02:49:25 +0100

    Remove console.log statement in fetchPosts function

--- js/fetch.js
+++ js/fetch.js
@@ -5,7 +5,6 @@ export async function fetchPosts(page = 1) {
     try {
         const response = await fetch(`${constant.url}/posts?_embed&status=publish&page=${page}`);
         const data = await response.json();
-        console.log(data);
         return data;
     } catch (error) {
         console.error('Error:', error);
