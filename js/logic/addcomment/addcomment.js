833f1c786d20905fa24f9b3e5c7e0acdc838ba48
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2024-03-07 20:00:59 +0100

    Update styles and add input floating label effect

--- js/logic/addcomment/addcomment.js
+++ js/logic/addcomment/addcomment.js
@@ -112,4 +112,20 @@ export function addNewComment() {
             input.addEventListener('input', () => handleInput(input));
         }
     });
+
+    // Input floating label effect
+    inputs.forEach(input => {
+        input.addEventListener('focus', () => {
+            const label = input.parentNode.querySelector('label');
+            const isDarkMode = document.body.classList.contains('dark-mode');
+            const colorStyle = isDarkMode ? 'color: var(--color-dark-text);' : '';
+            label.setAttribute('style', `transform: translate(15px, -30px); ${colorStyle}`);
+        });
+
+        input.addEventListener('blur', () => {
+            if (input.value === '') {
+                input.parentNode.querySelector('label').setAttribute('style', 'transform: translate(15px, 8px);');
+            }
+        });
+    });
 }
