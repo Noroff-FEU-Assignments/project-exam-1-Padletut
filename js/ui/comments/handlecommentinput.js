0eba918df473624a9ce6b5d1b6ac98ffa8e3c941
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2024-03-29 18:28:51 +0100

    Add email validation and remove error messages after 5 seconds

--- js/ui/comments/handlecommentinput.js
+++ js/ui/comments/handlecommentinput.js
@@ -1,4 +1,5 @@
 import { verifyFormInput } from '../shared/verifyforminput.js';
+import { validateValidEmail } from '../shared/validatevalidemail.js';
 
 // Function to handle comment form input
 export const handleCommentInput = (eventOrInput) => {
@@ -7,15 +8,22 @@ export const handleCommentInput = (eventOrInput) => {
     switch (input.id) {
         case 'author':
             errorMessage = 'Please enter your name';
+            removeErrorMessage(input);
             break;
         case 'email':
             errorMessage = 'Please enter your email';
+            removeErrorMessage(input);
+            if (input.value.length > 0 && !validateValidEmail(input.value)) {
+                errorMessage = 'Please enter a valid email address';
+            }
             break;
         case 'comment':
             errorMessage = 'Please enter a comment';
+            removeErrorMessage(input);
             break;
         default:
             errorMessage = 'Please fill out this field';
+            removeErrorMessage(input);
     }
     verifyFormInput(input, errorMessage);
 
@@ -24,3 +32,16 @@ export const handleCommentInput = (eventOrInput) => {
         input.removeEventListener('input', handleCommentInput);
     }
 };
+
+function removeErrorMessage(input) {
+    // Remove error messages after 5 seconds and remove input frame color
+    if (document.querySelector('.comments') && input.value.length === 0) {
+        setTimeout(() => {
+            const errorMessageElement = input.parentNode.querySelector('.form-error-message');
+            if (errorMessageElement) {
+                errorMessageElement.remove();
+                input.style.borderColor = '';
+            }
+        }, 5000);
+    }
+}
--- js/ui/contact/handlecontactinput.js
+++ js/ui/contact/handlecontactinput.js
@@ -9,33 +9,40 @@ export const handleContactInput = (eventOrInput) => {
         case 'name':
             if (input.value.length !== 0 && input.value.length < 6) {
                 errorMessage = 'Name must be at least 6 characters long';
-            } else {
+            } else if (input.value.length === 0) {
                 errorMessage = 'Please enter your name';
+                removeErrorMessage(input);
             }
             break;
         case 'email':
-            if (!validateValidEmail(input.value)) {
-                errorMessage = 'Please enter a valid email address';
-            } else {
+            if (input.value.length === 0 || input.addEventListener) {
                 errorMessage = 'Please enter your email';
-            }
+                removeErrorMessage(input);
+            } else if (!validateValidEmail(input.value)) {
+                errorMessage = 'Please enter a valid email address';
+            } else if (input.addEventListener('focus', () => removeErrorMessage(input)));
             break;
         case 'subject':
             if (input.value.length !== 0 && input.value.length < 15) {
                 errorMessage = 'Subject must be at least 16 characters long';
-            } else {
+            } else if (input.value.length === 0) {
                 errorMessage = 'Please enter a subject';
-            }
+                removeErrorMessage(input);
+            } else if (input.addEventListener('focus', () => removeErrorMessage(input)));
             break;
         case 'message':
             if (input.value.length !== 0 && input.value.length < 26) {
                 errorMessage = 'Message must be at least 26 characters long';
-            } else {
+            } else if (input.value.length === 0) {
                 errorMessage = 'Please enter a message';
-            }
+                removeErrorMessage(input);
+            } else if (input.addEventListener('focus', () => removeErrorMessage(input)));
             break;
         default:
             errorMessage = 'Please fill out this field';
+            if (input.value.length === 0) {
+                removeErrorMessage(input);
+            } else if (input.addEventListener('focus', () => removeErrorMessage(input)));
     }
     verifyFormInput(input, errorMessage);
 
@@ -44,3 +51,24 @@ export const handleContactInput = (eventOrInput) => {
         input.removeEventListener('input', handleContactInput);
     }
 };
+
+function removeErrorMessage(input) {
+    // Remove error messages after 5 seconds and remove input frame color
+    console.log(input.value.length);
+    setTimeout(() => {
+        const errorMessageElement = input.parentNode.querySelector('.form-error-message');
+        if (errorMessageElement) {
+            errorMessageElement.remove();
+            input.style.borderColor = '';
+        }
+    }, 5000);
+
+    // Remove error message if focus event is triggered
+    input.addEventListener('focus', () => {
+        const errorMessageElement = input.parentNode.querySelector('.form-error-message');
+        if (errorMessageElement) {
+            errorMessageElement.remove();
+            input.style.borderColor = '';
+        }
+    });
+}
