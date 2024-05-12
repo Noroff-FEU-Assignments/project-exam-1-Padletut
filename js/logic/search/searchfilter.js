7750d32cdf07c518edab11829a9b459ec2f0c578
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2024-05-12 21:11:29 +0200

    refactor: Improve search functionality and filter logic in searchFilter function

--- js/logic/search/searchfilter.js
+++ js/logic/search/searchfilter.js
@@ -1,15 +1,26 @@
-import { fetchAllPosts } from '../api/fetchallposts.js';
-
-/* Function to filter the search results */
+import { fetchAllPosts } from '../../logic/api/fetchallposts.js';
 
 export async function searchFilter(searchInput) {
-    const searchValue = searchInput.value.toLowerCase().trim();
+    let searchValues;
+    const trimmedInput = searchInput.value.toLowerCase().trim();
+
+    // Check if the search query is enclosed in quotes
+    if (trimmedInput.startsWith('"') && trimmedInput.endsWith('"')) {
+
+        searchValues = [trimmedInput.slice(1, -1)];
+    } else {
+        // Split the query into individual words
+        searchValues = trimmedInput.split(' ');
+    }
+
     const searchResults = [];
-    if (searchValue.length < 1) return;
+    if (searchValues.length < 1) return;
     const allPosts = await fetchAllPosts();
 
     allPosts.forEach(post => {
-        if (post.title.rendered.toLowerCase().includes(searchValue) || post.content.rendered.toLowerCase().includes(searchValue)) {
+        const postTitle = post.title.rendered.toLowerCase();
+        const postContent = post.content.rendered.toLowerCase();
+        if (searchValues.some(value => postTitle.includes(value) || postContent.includes(value))) {
             searchResults.push(post);
         }
     });
